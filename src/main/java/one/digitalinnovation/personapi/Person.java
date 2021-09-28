599185c61af7d3393917c48dbca0a079d6b18fe8
Jesaias Santos <jesasdev@gmail.com>
2021-09-27 23:21:23 -0300

    Projeto criado com as  classes Phone,Person e PersonController

---
+++ src/main/java/one/digitalinnovation/personapi/Person.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personapi;
+
+import one.digitalinnovation.personapi.entities.Phone;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones = new ArrayList<>();
+}
